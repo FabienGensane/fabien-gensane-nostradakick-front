8cd9b143b2e56c9c930c06d18df87b8e8fd14dc9
Yvan Trouillet <yvan.trouillet@gmail.com>
2025-02-11 14:58:36 +0100

    logique header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,40 @@
-// import { useEffect, useState } from "react";
-// import Header_mobile from "../Header-mobile/Header_mobile";
-// import Header_desktop from "../Header_desktop/Header_desktop";
-
+import { useEffect, useState } from "react";
+import Header_mobile from "../Header-mobile/Header_mobile";
+import Header_desktop from "../Header_desktop/Header_desktop";
 import Footer from "../Footer/Footer";
 import Header_desktop_logged from "../Header_desktop_logged/Header_desktop_logged";
+
 import "./App.scss";
 
 function App() {
-	// const [sizeWindow, setSizeWindow] = useState(375);
+	const [sizeWindow, setSizeWindow] = useState(0);
+	const [islogged, setIsLogged] = useState(false);
 
-	// useEffect(() => {
-	// 	setSizeWindow(window.innerWidth);
-	// }, []);
+	useEffect(() => {
+		const handleResize = () => {
+			setSizeWindow(window.innerWidth);
+		};
+
+		// Ajouter un écouteur d'événement pour "resize"
+		window.addEventListener("resize", handleResize);
+
+		// Supprimer l'écouteur d'événement pour "resize"
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	return (
 		<>
-			{/* {
-		sizeWindow <= 375 
-		? <Header_mobile /> 
-		: <Header_desktop />
-		} */}
-			<Header_desktop_logged />
-			<Footer />
+			{sizeWindow < 490 ? (
+				<Header_mobile />
+			) : islogged ? (
+				<Header_desktop_logged />
+			) : (
+				<Header_desktop />
+			)}
+
+			{sizeWindow > 490 && <Footer />}
 		</>
 	);
 }
