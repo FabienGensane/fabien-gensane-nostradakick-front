061c9a051cdfb4fe66dc6e4961f4433ecd06bc16
AbdelZahri <abdel.zahri@oclock.school>
2025-02-12 12:14:57 +0100

    recupération des données de prédiction

--- src/components/PredictsPage/PredictsPage.tsx
+++ src/components/PredictsPage/PredictsPage.tsx
@@ -2,15 +2,34 @@ import { Search } from "./Search/Search";
 import Predict_Card from "./Predict_Card/Predict_Card";
 
 import "./PredicstPage.scss";
+import { useEffect, useState } from "react";
 
 export const PredictsPage = () => {
+
+	const [predicts, setPredicts] = useState([]);
+
+	const fetchPredicts = async () => {
+		try {
+			const response = await fetch("http://localhost:3000/api/predictions");
+			const data = await response.json();
+			console.log(data);
+			setPredicts(data);
+		} catch (error) {
+			console.log('erreur');
+		}
+	}
+	
+	useEffect (()=> {	
+		fetchPredicts();
+	}, [])
+	
+
+	console.log(predicts)
+	
 	return (
 		<main className="main__predicts">
 			<Search />
 			<div className="main__predicts__container">
-                <Predict_Card />
-                <Predict_Card />
-                <Predict_Card />
                 <Predict_Card />
 				
             </div>
